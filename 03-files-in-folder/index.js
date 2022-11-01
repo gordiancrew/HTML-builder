1922f8814aba57fa8d25db1b89242ea51ab47e85
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-02 01:46:49 +0300

    fix: processing solution task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const pathControl=path.join(__dirname,'/secret-folder/');
+
+fs.readdir(pathControl, { withFileTypes: true },
+    (err, files) => {
+        console.log("\nCurrent files:");
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                console.log( path.extname(file.name));
+
+
+
+
+                fs.stat(path.join(pathControl,file.name), (error, stats) => {
+                    if (error) {
+                      console.log(error);
+                    }
+                    else {
+                      console.log("Stats object for: example_file.txt");
+                      console.log(stats);
+                    
+                      // Using methods of the Stats object
+                      console.log("Path is file:", stats.isFile());
+                      console.log("Path is directory:", stats.isDirectory());
+                    }
+                  });
+
+
+            })
+        }
+    })
