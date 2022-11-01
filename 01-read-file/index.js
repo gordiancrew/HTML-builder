607d2996e8c7417a73b1b9022f1deabff33b27cc
Uladzimir Kazantsau <gordiancrew@gmail.com>
2022-11-01 16:36:45 +0300

    fix: solution task 1 (update)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,8 +4,5 @@ const path = require('path');
 
 let stream=new fs.ReadStream(path.join(__dirname,'text.txt'), {encoding: 'utf-8'});
 
-stream.on('readable', function(){
-    let data = stream.read();
-    if(data != null)console.log(data);
-});
+stream.on('data', chunk => console.log(chunk));
  
