1ac5221eccebc1710d469366b40e5521c4dad271
Uladzimir Kazantsau <gordiancrew@gmail.com>
2022-11-01 18:09:09 +0300

    fix: processing in solution task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,3 +1,31 @@
 console.log("Start");
 
-const path=require('path');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    promt: '>'
+
+});
+console.log("Hello my frient! Enter your text:")
+rl.prompt();
+rl.on('line', (input) => {
+    if (input != "exit") {
+
+
+        fs.appendFile(path.join(__dirname, 'text.txt'), input, (err) => {
+            if (err) throw err;
+            console.log('Data has been added!');
+        });
+    }
+    rl.close();
+    console.log("Good buy!!");
+});
+
+
+
