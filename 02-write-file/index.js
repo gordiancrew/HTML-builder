424fbf52fcbb6c9a889ca971caef5ed6162122b3
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-02 00:23:34 +0300

    fix: solution task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,29 +3,26 @@ console.log("Start");
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-
-const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+let outp = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 let rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
-    promt: '>'
+    output: process.stdout
 
 });
-console.log("Hello my frient! Enter your text:")
+console.log("Привет друг! Введи текст для записи в файл:")
 rl.prompt();
 rl.on('line', (input) => {
     if (input != "exit") {
-
-
         fs.appendFile(path.join(__dirname, 'text.txt'), input, (err) => {
             if (err) throw err;
-            console.log('Data has been added!');
+            console.log('Записали! Еще что-нибудь запишем? Введи еще текст:');
         });
+    } else {
+        rl.close();
     }
-    rl.close();
-    console.log("Good buy!!");
 });
+rl.on('close', () => console.log("Процесс окончен! До новых встреч!"));
 
 
 
