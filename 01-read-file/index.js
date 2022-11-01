e1357dc37e11ad8aa3fda4c193ab3a66f1ece09f
Uladzimir Kazantsau <gordiancrew@gmail.com>
2022-11-01 16:01:37 +0300

    fix: solution task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+
+let stream=new fs.ReadStream(path.join(__dirname,'text.txt'), {encoding: 'utf-8'});
+
+stream.on('readable', function(){
+    let data = stream.read();
+    if(data != null)console.log(data);
+});
+ 
