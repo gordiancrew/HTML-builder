1d1fb740630c057faba67d846b15c7bfba2736de
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-02 23:08:41 +0300

    fix: solution task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,34 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const pathControl=path.join(__dirname,'/secret-folder/');
+const pathControl = path.join(__dirname, '/secret-folder/');
 
 fs.readdir(pathControl, { withFileTypes: true },
-    (err, files) => {
-        console.log("\nCurrent files:");
-        if (err)
-            console.log(err);
-        else {
-            files.forEach(file => {
-                // console.log( path.extname(file.name));
-
-
-
-
-                fs.stat(path.join(pathControl,file.name), (error, stats) => {
-                    if (error) {
-                      console.log(error);
-                    }
-                    else {
-                     
-                      console.log("--"+file.name+"--"+stats.size)
-                    
-                      // Using methods of the Stats object
-                      // console.log("Path is file:", stats.isFile());
-                      // console.log("Path is directory:", stats.isDirectory());
-                    }
-                  });
-
-
-            })
-        }
-    })
+  (err, files) => {
+    console.log("FILES:");
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.stat(path.join(pathControl, file.name), (error, stats) => {
+          if (error) {
+            console.log(error);
+          }
+          else {
+            let arrName = file.name.split(".");
+            console.log(arrName[0] + " - " + arrName[1] +
+              " - " + stats.size / 1000 + "kb");
+          }
+        });
+      })
+    }
+  })
