99a812790cd97974d076cac920d661be8bcb86d1
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-03 22:39:28 +0300

    feat: solution task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,12 +3,23 @@ const path = require('path');
 const pathControl = path.join(__dirname, '/styles/');
 const pathBundle = path.join(__dirname, '/project-dist/bundle.css');
 
+
 fs.readdir(pathControl, { withFileTypes: true },
     (err, files) => {
         console.log("FILES:");
         if (err)
             console.log(err);
         else {
+            fs.stat(pathBundle, function (err) {
+                if (!err) {
+                    console.log("Директория есть");
+                    fs.truncate(pathBundle, err => {
+                        if (err) throw err; // не удалось очистить файл
+                        console.log("Файл успешно очищен");
+                    });
+                }
+            });
+
             files.forEach(file => {
                 fs.stat(path.join(pathControl, file.name), (error, stats) => {
                     if (error) {
