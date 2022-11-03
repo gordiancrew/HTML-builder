e9bee4f95ba4900ed8e7b018abe5b7d8bab0abc3
Uladzimir Kazantsau <gordiancrew@gmail.com>
2022-11-03 14:50:16 +0300

    feat: processing solution task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const pathControl = path.join(__dirname, '/styles/');
+const pathBundle = path.join(__dirname, '/project-dist/bundle.css');
+
+fs.readdir(pathControl, { withFileTypes: true },
+    (err, files) => {
+        console.log("FILES:");
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                fs.stat(path.join(pathControl, file.name), (error, stats) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        let arrName = file.name.split(".");
+                        if (arrName[1] === "css") {
+                            let stream = new fs.ReadStream(path.join(pathControl, file.name), { encoding: 'utf-8' });
+                            stream.on('data', chunk => fs.appendFile(pathBundle, chunk, (err) => {
+                                if (err) throw err;
+                                console.log("файл " + file.name + " добавлен в бандл!");
+                            }));
+                        }
+                    }
+                });
+            })
+        }
+    })
