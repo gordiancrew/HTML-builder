e10f92d77d2c09d2c0337e470cd1764e31412ab2
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-05 02:04:30 +0300

    feat: task 6 processing, add copy asset and styles bundle

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,17 +1,138 @@
 const fs = require('fs');
 const path = require('path');
-const pathThemplate= path.join(__dirname, 'template.html');
+const pathThemplate = path.join(__dirname, 'template.html');
+const pathStylesFrom = path.join(__dirname, '/styles/');
+const pathStylesTo = path.join(__dirname, '/project-dist/style.css');
+const pathDist = path.join(__dirname, '/project-dist/');
+const pathAssetsFrom = path.join(__dirname, '/assets/');
+const pathAssetsTo = path.join(__dirname, '/project-dist/assets');
 
-let str='';
-let stream=new fs.ReadStream(pathThemplate, {encoding: 'utf-8'});
 
-stream.on('data', chunk => str+=chunk);
 
-stream.on('end',()=>{
-   
-   
-   str= str.replace("{{der}}","{{heheder}}");
-    
-    
-    console.log(str)
+
+function copyDir(pathFrom, pathTo) {
+    fs.readdir(pathFrom, { withFileTypes: true },
+        (err, files) => {
+            files.forEach(file => {
+
+
+
+
+                fs.stat(path.join(pathFrom, file.name), function (err, stats) {
+                    if (!(stats.isFile())) {
+                        console.log('это папка');
+                        fs.mkdir(path.join(pathTo, file.name), err => {
+                            if (err) throw err;
+                            console.log("Папка " + path.join(pathTo, file.name) +
+                                " успешно создана!");
+                            copyDir(path.join(pathFrom, file.name), path.join(pathTo, file.name));
+                        });
+
+
+
+
+
+
+                    }
+                    else{
+                        console.log('это файл');
+
+
+                        fs.copyFile(path.join(pathFrom, file.name), path.join(pathTo, file.name), err => {
+                            if (err) throw err;
+                            console.log("Файл " + file.name + " из папки " +
+                                pathFrom + " копирован в папку " + pathTo);
+                        });
+
+
+
+
+
+                    }
+                });
+
+            })
+        })
+
+}
+
+function copyStyles() {
+    fs.readdir(pathStylesFrom, { withFileTypes: true },
+        (err, files) => {
+            console.log("FILES:");
+            if (err)
+                console.log(err);
+            else {
+                fs.stat(pathStylesTo, function (err) {
+                    if (!err) {
+                        console.log("Директория style.css есть");
+                        fs.truncate(pathStylesTo, err => {
+                            if (err) throw err; // не удалось очистить файл
+                            console.log("Файл style.css успешно очищен");
+                        });
+                    }
+                });
+
+                files.forEach(file => {
+                    fs.stat(path.join(pathStylesFrom, file.name), (error, stats) => {
+                        if (error) {
+                            console.log(error);
+                        }
+                        else {
+                            let arrName = file.name.split(".");
+                            if (arrName[1] === "css") {
+                                let stream = new fs.ReadStream(path.join(pathStylesFrom, file.name), { encoding: 'utf-8' });
+                                stream.on('data', chunk => fs.appendFile(pathStylesTo, chunk, (err) => {
+                                    if (err) throw err;
+                                    console.log("файл " + file.name + " добавлен в бандл!");
+                                }));
+                            }
+                        }
+                    });
+                })
+            }
+        })
+
+}
+
+
+
+fs.mkdir(pathDist, err => {
+    if (err) throw err;
+    console.log("Папка " + pathDist +
+        " успешно создана!");
+    fs.mkdir(pathAssetsTo, err => {
+        if (err) throw err;
+        console.log("Папка " + pathAssetsTo +
+            " успешно создана!");
+       
+       
+            copyDir(pathAssetsFrom, pathAssetsTo);
+        copyStyles();
+
+
+    });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let str = '';
+// let stream = new fs.ReadStream(pathThemplate, { encoding: 'utf-8' });
+
+// stream.on('data', chunk => str += chunk);
+// stream.on('end', () => {
+//     str = str.replace("{{header}}", "{{heheder}}");
+//     console.log(str)
+// });
